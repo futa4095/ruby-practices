25e1e29681d9ef585696e47c8bbb7485560c1b4b
futa4095 <kick20000@gmail.com>
2021-05-03 20:59:13 +0900

    インスタンス変数とローカル変数の混同を回避する

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -8,30 +8,30 @@ class Game
 
   def initialize(throwing_result)
     marks = throwing_result.split(',')
-    @frames = build_frames(marks)
+    build_frames(marks)
   end
 
   def build_frames(marks)
-    frames = Array.new(9) do
+    @frames = Array.new(9) do
       first_mark = marks.shift
       second_mark = first_mark == 'X' ? nil : marks.shift
       Frame.new first_mark, second_mark
     end
-    frames << Frame.new(*marks)
+    @frames << Frame.new(*marks)
   end
 
   def score
-    9.times.sum(frames.last.score) do |i|
-      if frames[i].strike?
-        next_frame = frames[i.next]
+    9.times.sum(@frames.last.score) do |i|
+      if @frames[i].strike?
+        next_frame = @frames[i.next]
         after_shots = [next_frame.first_shot, next_frame.second_shot]
-        next_next_frame = frames[i.next.next]
+        next_next_frame = @frames[i.next.next]
         after_shots += [next_next_frame.first_shot] if next_next_frame
-        frames[i].score + after_shots.compact.take(2).sum(&:score)
+        @frames[i].score + after_shots.compact.take(2).sum(&:score)
       elsif frames[i].spare?
-        frames[i].score + frames[i.next].first_shot.score
+        @frames[i].score + @frames[i.next].first_shot.score
       else
-        frames[i].score
+        @frames[i].score
       end
     end
   end
