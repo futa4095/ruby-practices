32992d44d0024fed61a4ba269aee1c35c6bdb0a3
futa4095 <kick20000@gmail.com>
2021-05-16 18:57:47 +0900

    nameをインスタンス変数からメソッドにする

--- 09.wc_object/lib/total_counter.rb
+++ 09.wc_object/lib/total_counter.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class TotalCounter
-  attr_reader :name, :number_of_lines, :number_of_words, :number_of_bytes
+  TITLE = 'total'
+
+  attr_reader :number_of_lines, :number_of_words, :number_of_bytes
 
   def initialize
-    @name = 'total'
     @number_of_lines = 0
     @number_of_words = 0
     @number_of_bytes = 0
+  end
+
+  def name
+    TITLE
   end
 
   def accumlate(another_counter)
